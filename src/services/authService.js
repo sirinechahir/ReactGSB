02395b1168e0e79fada2aae3de5710c7c9f872a9
chahir <sirine.chahir@csilyon.fr>
2026-06-02 14:25:31 +0200

    mission finie et déployée pour E6

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 //export const API_URL = 'http://gsb.julliand.etu.lmdsio.com/api/';
-export const API_URL = 'http://localhost:8000/api/';
+//export const API_URL = 'http://localhost:8000/api/';
+export const API_URL = 'http://gsb.chahir.etu.lmdsio.com/api/';
 
 export const signIn = async (login, password) => {
     const response = await axios.post(`${API_URL}visiteur/auth`,
